a42efac1e2a8e5737c0502c16343f6c5a7f892e1
KesaviAravinthan <kesaviap@gmail.com>
2022-05-30 11:21:32 +0530

    navbar for teacher page added

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
-import AddAnnouncement from './components/teacher/addAnnouncement';
-import ViewStudentDetail from './components/teacher/viewStudentDetail';
-import AddExamResult from './components/teacher/addExamResults';
+import NavBarTeacher from './components/teacher/navbar';
 
 export default function App() {
-  
-
   return (
     <div className="App">
-      <AddAnnouncement></AddAnnouncement>
-      <ViewStudentDetail> </ViewStudentDetail>
-      <AddExamResult></AddExamResult>
+      <NavBarTeacher/>
     </div>
   );
 }
