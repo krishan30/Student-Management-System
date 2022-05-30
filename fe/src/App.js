bbd0c2cc36af8f67c2b6af87acf18add8f42db0c
KasunAb <kasunssckinfo@gmail.com>
2022-05-30 16:36:18 +0530

    5th

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,51 +1,50 @@
-import React from "react";
-<<<<<<< HEAD
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.css";
-import AddAnnouncement from './components/teacher/addAnnouncement';
-import ViewStudentDetail from './components/teacher/viewStudentDetail';
-import AddExamResult from './components/teacher/addExamResults';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import NewEnroll from "./pages/new_Enroll/NewEnroll";
+import Grades from "./pages/grades/Grades";
+import Profile from "./pages/profile/Profile";
+import MyCourses from "./pages/my_Courses/MyCourses.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
-export default function App() {
-  
-=======
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Teacher from "./pages/admin/adminTeacher";
-import AdminCourse from "./pages/admin/adminCourse";
-import AdminStudent from "./pages/admin/adminStudent";
-import AdminHeader from "./components/adminHeader";
-import Sidebar from "./components/adminSideBar";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import AdminTeacher from "./pages/admin/adminTeacher";
-
->>>>>>> test
+function App() {
+  const { darkMode } = useContext(DarkModeContext);
 
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <AddAnnouncement></AddAnnouncement>
-      <ViewStudentDetail> </ViewStudentDetail>
-      <AddExamResult></AddExamResult>
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/">
+
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+
+            <Route path="grades">
+              <Route index element={<Grades />} />
+            </Route>
+
+            <Route path="newEnroll">
+              <Route index element={<NewEnroll />} />
+            </Route>
+
+            <Route path="profile">
+              <Route index element={<Profile inputs={productInputs} title="Add New Product" />} />
+            </Route>
+
+            <Route path="myCourses">
+              <Route index element={<MyCourses />} />
+            </Route>
+
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </div>
-=======
-    <Router>
-        <AdminHeader />
-      <div className="row m-0">
-        <div className="col-2 p-0">
-          <Sidebar />
-        </div>
-        <div className="col-9">
-          <Routes>
-            <Route path="/Admin/dashboard" element={<Teacher />} />
-            <Route path="/Admin/teacher" element={<AdminTeacher />} />
-            <Route path="/Admin/course" element={<AdminCourse />} />
-            <Route path="/Admin/student" element={<AdminStudent />} />
-            
-          </Routes>
-        </div>
-      </div>
-    </Router>
->>>>>>> test
   );
 }
+
+export default App;
