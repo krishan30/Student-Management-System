f5d144449b5e4c659c3c52447f5c58864c3b1f3f
KesaviAravinthan <kesaviap@gmail.com>
2022-05-30 14:15:47 +0530

    bootstrap installed

--- fe/src/components/teacher/addExamResults.js
+++ fe/src/components/teacher/addExamResults.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+// import { Container, Row, Col, Button } from "react-bootstrap";
 
 const ExamResult = () => {
   const data = [
